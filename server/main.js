1f5eb11a0385325b052da80b38258d1434f7840a
Javier Acevedo <javier.acevedohdz@hotmail.com>
2024-05-08 10:05:28 -0600

    Añade archivos backend para Socket Añade archivos de la carpeta public y modifica el main.js de server

--- server/main.js
+++ server/main.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var app = express();
-
-/*Como trabajamos con socket, es recomendable usar el módulo HTTP
-para pasarle la app a express y manejar bien http*/
 var server = require('http').Server(app);
-/* aquí estará toda la funcionalidad de los sockets
-   se requiere la librería socket.io
-   se pasa la variable Server que tiene la app express y HTTP */
 var io = require('socket.io')(server);
+
+/* Usamos un middleware para usar elementos estáticos en la sección pública de la aplicación */
+app.use(express.static('public'));
 
 app.get('/',function(req, res){
     res.status(200).send("Hola Mundo");
 });
 
+/* De esta forma activamos socket para que esté escuchando 
+mandamos un mensaje de control por consola para saber que pasa
+y tneemos que hacer el mensaje venga del navegador web mediante HTML y JS */
+io.on('connection', function(socket){
+    console.log('Alguien se ha conectado con socket')
+});
+
 server.listen(3002, function(){
-    console.log("El servidor está corriendo en http://localhost:3003");
+    console.log("El servidor está corriendo en http://localhost:3002");
 });
