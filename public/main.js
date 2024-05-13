2a8c9bd656a6bd26e1281d53aee7bb837373ea5a
LuisFernandoH <luisspartan10@gmail.com>
2024-05-13 09:55:50 -0600

    Se finaliza la version Beta

--- public/main.js
+++ public/main.js
@@ -7,14 +7,23 @@ socket.on('messages', function(data){
 });
 
 function render(data){
-    var html = `<div>
-                    <strong>${data.autor}</strong>:
-                    <em>${data.texto}</em>
-                    </div>`;
+    //Restructuramos esta seccion para que se maneje el array
+    //elem es un conjunto de cosas
+    //con ma recorremos el array
+
+    var html = data.map(function(elem, index){
+            return(`<div>
+                    <strong>${elem.autor}</strong>:
+                    <em>${elem.texto}</em>
+                    </div>`);
+    }).join(" ");
 
                 document.getElementById('messages').innerHTML = html;
 }
 
+//Cada ves que alguien precione el boton enviar en el formulario
+//El cliente emite un nuevo mensaje y manda el payload
+//Culminar el proyecto a una version Beta
 function addMessage(e){
     var payload = {
         autor: document.getElementById(username).value,
--- server/main.js
+++ server/main.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+//Array  que guarda los mensajes
+var messages = [{
+    id: 1,
+    texto: "Welcome Stranger!",
+    autor: "Luis Fernando Hernandez Barraza"
+}];
 
 /* Usamos un middleware para usar elementos estáticos en la sección pública de la aplicación */
 app.use(express.static('public'));
@@ -12,14 +18,15 @@ app.get('/',function(req, res){
 
 /* De esta forma activamos socket para que esté escuchando 
 mandamos un mensaje de control por consola para saber que pasa
-y tneemos que hacer el mensaje venga del navegador web mediante HTML y JS */
+y tenemos que hacer el mensaje venga del navegador web mediante HTML y JS */
 io.on('connection', function(socket){
     console.log('Alguien se ha conectado con socket')
-    /*Aqui controlaremos los eventos del cliente mediante sockets */
-    socket.emit('messages', {
-        id: 1,
-        texto: "Welcome Stranger!",
-        autor: "Luis Fernando Hernandez Barraza"
+    socket.emit('messages', messages);
+    //ahora queremos escuchar los mensajes mandados por el cliente
+    socket.on('new-message', function(data){
+        messages.push(data);
+        //queremos que todos los mensajes se manden a todos los clientes
+        io.sockets.emit('messages', messages);
     });
 });
 
